8f622a64cdeae7104ca00695c53a9c597159db16
kieu-linh <you@example.com>
2021-08-14 22:39:08 +0700

    linh123

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function hamDropdown(i) {
+    if (i == 1) {
+        document.getElementById("hidden").classList.toggle("hienThi");
+    }
+    if (i == 2) {
+        document.getElementById("hidden-2").classList.toggle("hienThi");
+    }
+    if (i == 3) {
+        document.getElementById("hidden-3").classList.toggle("hienThi");
+    }
+    if (i == 4) {
+        document.getElementById("hidden-4").classList.toggle("hienThi");
+    }
+    if (i == 5) {
+        document.getElementById("hidden-5").classList.toggle("hienThi");
+    }
+    if (i == 6) {
+        document.getElementById("hidden-6").classList.toggle("hienThi");
+    }
+    if (i == 7) {
+        document.getElementById("hidden-7").classList.toggle("hienThi");
+    }
+    if (i == 8) {
+        document.getElementById("hidden-8").classList.toggle("hienThi");
+    }
+    if (i == 9) {
+        document.getElementById("hidden-9").classList.toggle("hienThi");
+    }
+    if (i == 10) {
+        document.getElementById("hidden-10").classList.toggle("hienThi");
+    }
+}
